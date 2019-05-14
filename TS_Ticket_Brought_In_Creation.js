3558d361af9032d96933106f740694f9aad67147
Babylon <xbettybabylonx@gmail.com>
2019-05-13 21:57:34 -0700

    need to get linking of div to # on page to work

---
+++ TS_Ticket_Brought_In_Creation.js
@@ -0,0 +1,21 @@
+var Client_ID
+var Tech_First_Name
+var Tech_Last_Name
+var Type_Of_Tech
+
+var Customer_First_Name
+var Customer_Last_Name
+var Customer_Phone
+var Customer_Email
+
+var PID
+var Box _Type
+var Box_Source
+var Date_Accquired
+var Current_OS
+var Additional_Items_Checked_In
+
+var Summary_Of_Issue
+var Detailed_Description_Of_Issue
+var Backup_Data
+var Quoted_Price
